7b99a3ac030e411afe63fc33a7904cbb56fa1683
lumijoe <spacedesign63@gmail.com>
2024-07-12 21:29:54 +0900

    test

---
+++ data_processing.py
@@ -0,0 +1,17 @@
+#初めてのファイルです
+money=10000
+count=68
+snack=400
+
+if money>count*snack:
+    print("You can buy it.")
+
+elif money>=snack*count:
+    print("You can buy but your money will no apear foever.")
+
+else:
+
+    print("Sorry your place no here.")
+
+
+
